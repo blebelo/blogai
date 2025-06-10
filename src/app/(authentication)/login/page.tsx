b00d6ade816850cd8860d035470beedd3618a93e
Benny <tarr184pro@gmail.com>
2025-06-10 15:36:44 +0200

    Added Login Page

---
+++ src/app/(authentication)/login/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+import React from "react";
+import type { FormProps } from "antd";
+import { Button, Form, Input, Typography } from 'antd';
+import { useStyles } from "./style";
+
+
+const SignUp: React.FC = () => {
+    const {styles} = useStyles()
+
+    type FieldType = {
+        email: string,
+        password: string
+        }
+
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div className={styles.Container}>
+            <Form
+                name="basic"
+                className={styles.Form}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Typography className={styles.Typography}>Log In</Typography>
+                <div className={styles.FormItems}>
+                    <label>Email</label>
+                    <Form.Item<FieldType>
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email' }]}
+                    >
+                        <Input  className={styles.Input}/>
+                    </Form.Item>
+
+                    <label>Password</label>
+                    <Form.Item<FieldType>
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password className={styles.Input}/>
+                    </Form.Item>
+                </div>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className={styles.Submit}>
+                        Log In
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+
+
+    )
+}
+
+export default SignUp;
